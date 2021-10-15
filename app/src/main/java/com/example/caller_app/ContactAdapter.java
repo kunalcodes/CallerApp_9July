8dc8683af13765874f03106e65e7dd29454ef5b2
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-10-15 14:18:06 +0530

    added new comments to adapter methods

--- app/src/main/java/com/example/caller_app/ContactAdapter.java
+++ app/src/main/java/com/example/caller_app/ContactAdapter.java
@@ -14,11 +14,16 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactViewHolder> {
     private ArrayList<Contact> contactList;
     private ItemClickListener itemClickListener;
 
+    /* adapter is created from this constructor and list is passed from the activity
+    which is initialized here
+     */
     public ContactAdapter(ArrayList<Contact> contactArrayList, ItemClickListener itemClickListener){
         this.itemClickListener = itemClickListener;
         this.contactList = contactArrayList;
     }
 
+
+    //this method inflates the layout for individual item
     @NonNull
     @Override
     public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -26,12 +31,14 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactViewHolder> {
         return new ContactViewHolder(view, itemClickListener);
     }
 
+    //here data from the list is bound to the created views
     @Override
     public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
         Contact contact = contactList.get(position);
         holder.setData(contact);
     }
 
+    //returns the list size so recyclerview knows the size
     @Override
     public int getItemCount() {
         return contactList.size();
